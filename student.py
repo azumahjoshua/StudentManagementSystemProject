b181a2e269d4d7ee7283889912d8f35217330f5f
azumahjoshua <joshuaazumah1086@gmail.com>
2023-01-14 16:00:55 +0100

    corrections

--- student.py
+++ student.py
@@ -1,20 +1,23 @@
-
 class Student:
     def __init__(self,students=[]):
         self.students = students
 
     def add_student(self):
-        # Read two inputs from the user, separated by a space
-        student_name = input("Enter Student Name: ")
-        student_id = input("Enter Student Id: ")
+       
         # Validate the inputs: make sure that the first input is a string and the second is an integer
-        if not isinstance(student_name, str) or not student_id.isdigit():
-            raise ValueError('Invalid input')
-        else:
-            name = student_name
-            student_id = student_id
-            student = {"name": name, "student_id": student_id}
-            self.students.append(student)
+        while True:
+             # Read two inputs from the user, separated by a space
+            student_name = input("Enter Student Name: ")
+            student_id = input("Enter Student Id: ")
+            if not isinstance(student_name, str) or not student_id.isdigit():
+               print('Invalid input try again.')
+               continue
+            else:
+                name = student_name
+                student_id = student_id
+                student = {"name": name, "student_id": student_id}
+                self.students.append(student)
+                break
 
     def view_students(self):
         print("-------------------View Student-----------------")
@@ -29,21 +32,16 @@ class Student:
                 return student
         return None
 
+ # search student by name
+    def search_student_by_name(self, name):
+        for student in self.students:
+            if student['name'] == name:
+                return student
+        return None
+
     # remove student by id
     def delete_student(self, student_id):
-        """"
-        In the delete_student method, the enumerate() function is used in combination with a for 
-        loop to iterate through the list of students. The enumerate() function adds a counter 
-        to an iterable and returns it in a form of enumerate object, which is an iterator containing pairs 
-        (index, element). This allows you to both access the elements of the list, as well as 
-        keep track of their index within the list.
-        When you iterate over the students list using the for loop, the enumerate()
-        function adds a counter variable to each iteration. In this case, 
-        i is the counter variable and it holds the current index of the student in the students list.
-        This is useful in this case because it allows to find the index of the student object that is 
-        to be deleted using the student_id passed to the function. Once we know the index, 
-        we can then remove that student object from the list using the del statement and the index of the element.
-        """
+
         # Check if student_id is an integer
         try:
             for student in self.students:
@@ -52,8 +50,8 @@ class Student:
                     print("Student has been removed.")
                     return
             raise ValueError(f"Student with ID {student_id} not found")
-        except ValueError as e:
-            print(e)
+        except ValueError:
+            print("input data is wrong")
 
     # get total number of students
     def get_num_students(self):
@@ -92,11 +90,11 @@ if __name__ == '__main__':
         elif option == 2:
             students.view_students()
         elif option == 3:
-            print("-------------------View Searchecd Student-----------------")
+            print("-------------------View Searched Student-----------------")
             search_student_id = input('Enter id of student to be searched: ')
             student = students.search_student(search_student_id)
             if student:
-                print("-------------------View Searchecd Student-----------------")
+                print("-------------------View Searched Student-----------------")
                 print(f"Student with ID {search_student_id} found: {student['name']}")
             else:
                 print("Student was not found")
@@ -107,40 +105,9 @@ if __name__ == '__main__':
         elif option == 5:
             print(f"Number students: {students.get_num_students()}")
         elif option == 6:
-            print('Thanks message before exiting')
+            print('goodbye!')
             exit()
         else:
-            print('Invalid option. Please enter a number between 1 and 4.')
+            print('Invalid option. Please enter a number between 1 and 6.')
 
 
-# class TestStudentMethods(unittest.TestCase):
-# 
-    # def setUp(self):
-        # self.students = Student()
-# 
-    # def test_add_student(self):
-        # self.students.add_student()
-        # self.assertEqual(len(self.students.students), 1)
-        # self.students.add_student()
-        # self.assertEqual(len(self.students.students), 2)
-# 
-    # def test_search_student(self):
-        # self.students.add_student()
-        # self.assertIsNotNone(self.students.search_student("1"))
-        # self.assertIsNone(self.students.search_student("2"))
-# 
-    # def test_remove_student(self):
-        # self.students.add_student()
-        # self.assertEqual(len(self.students.students), 1)
-        # self.students.remove_student("1")
-        # self.assertEqual(len(self.students.students), 0)
-# 
-    # def test_get_num_students(self):
-        # self.students.add_student()
-        # self.assertEqual(self.students.get_num_students(), 1)
-        # self.students.add_student()
-        # self.assertEqual(self.students.get_num_students(), 2)
-# 
-# 
-# if __name__ == '__main__':
-    # unittest.main()
